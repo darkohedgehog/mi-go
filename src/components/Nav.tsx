39ee75a697d76a8a132df082d672f3a40389525d
Darko Živić <zivic.darko79@gmail.com>
2024-11-07 15:06:14 +0100

    Remove "bg-gray-50" class from nav element.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,7 +4,7 @@ const Nav = () => {
   return (
     
 
-<nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+<nav className="border-gray-200 dark:border-gray-700">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
         <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
