a442272d713a63aa727f4d8b9b8dc039a0d60297
Darko Živić <zivic.darko79@gmail.com>
2024-11-07 18:21:02 +0100

    "Added NavData translations, react-icons, and refactored Nav component"

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,42 +1,62 @@
-import React from 'react'
-import Logo from './logo/Logo'
-import Link from 'next/link'
+"use client"
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useTranslations } from 'next-intl';
+
+// icons
+import {
+  HiHome,
+  HiUser,
+  HiEnvelope,
+} from 'react-icons/hi2';
+import { TfiAnnouncement } from 'react-icons/tfi';
+
 
 const Nav = () => {
+  const pathname = usePathname();
+  const pathSegments = pathname.split('/');
+  const currentLocale = pathSegments[1] || 'hr'; // Default to 'hr' if no locale is found
+
+  const t = useTranslations('NavData');
+  // nav data
+  const navData = [
+    { name: `${t('home')}`, path: '/', icon: <HiHome /> },
+    { name: `${t('about')}`, path: '/about', icon: <HiUser /> },
+    { name: `${t('shop')}`, path: '/shop', icon: <TfiAnnouncement /> },
+    { name: `${t('contact')}`, path: '/contact', icon: <HiEnvelope /> },
+  ];
+
+
   return (
-    
+    <nav className='flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'>
+      {/* inner */}
+      <div className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full text-gray-300'>
+        {navData.map((link, index) => {
+          const localizedPath = `/${currentLocale}${link.path === '/' ? '' : link.path}`;
 
-<nav className="border-gray-200 dark:border-gray-700">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-        <Logo />
-    </Link>
-    <button data-collapse-toggle="navbar-solid-bg" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
-        <span className="sr-only">Open main menu</span>
-        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-    <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-      <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-        <li>
-          <a href="#" className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Home</a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+          return (
+            <Link
+              href={localizedPath}
+              key={index}
+              prefetch={true}
+              className={`${localizedPath === pathname && 'text-accent dark:text-accentDark'} relative flex items-center group hover:text-accent transition-all duration-300`}
+            >
+              {/* tooltip */}
+              <div className='absolute pr-14 right-0 hidden xl:group-hover:flex'>
+                <div className='bg-white relative flex text-accent items-center p-[6px] rounded-[3px]'>
+                  <div className='text-[12px] leading-none font-semibold'>{link.name}</div>
+                  {/* triangle */}
+                  <div className='border-solid border-l-white border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2'></div>
+                </div>
+              </div>
+              {/* icon */}
+              <div>{link.icon}</div>
+            </Link>
+          );
+        })}
+      </div>
+    </nav>
+  );
+};
 
-  )
-}
-
-export default Nav
+export default Nav;
