36737f064460b14c9f14203eecb954277ec06d0a
Darko Živić <zivic.darko79@gmail.com>
2024-11-14 19:53:20 +0100

    "Added @tabler/icons-react dependency, updated ContactPage component, and modified tailwind.config.ts"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,15 @@ const config: Config = {
         fifth: "moveInCircle 20s ease infinite",
         orbit: "orbit calc(var(--duration)*1s) linear infinite", // nova animacija
       },
+      boxShadow: {
+        derek: `0px 0px 0px 1px rgb(0 0 0 / 0.06),
+        0px 1px 1px -0.5px rgb(0 0 0 / 0.06),
+        0px 3px 3px -1.5px rgb(0 0 0 / 0.06), 
+        0px 6px 6px -3px rgb(0 0 0 / 0.06),
+        0px 12px 12px -6px rgb(0 0 0 / 0.06),
+        0px 24px 24px -12px rgb(0 0 0 / 0.06)`,
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+      },
       keyframes: {
         moveHorizontal: {
           "0%": {
