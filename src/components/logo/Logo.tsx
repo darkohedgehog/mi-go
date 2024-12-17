b4ce20a8070890964e450981e85b4fc32792bee2
Darko Živić <zivic.darko79@gmail.com>
2024-12-17 19:20:20 +0100

    `Added usePathname hook and Link component to Logo component`

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,9 +1,18 @@
+"use client"
 import Image from 'next/image';
-import React from 'react'
+import React from 'react';
+import { usePathname } from "next/navigation";
+import Link from 'next/link';
+
 
 const Logo = () => {
+  const pathname = usePathname();
+  const pathSegments = pathname.split("/");
+  const currentLocale = pathSegments[1] || "hr";
   return (
     <div>
+      <Link
+      href={`/${currentLocale}/`}>
         <Image
             src={'/logo.png'}
             className="h-20 w-20 flex justify-start items-center -rotate-12"
@@ -12,6 +21,7 @@ const Logo = () => {
             height={40}
             priority={true}
           />
+          </Link>
     </div>
   )
 }
